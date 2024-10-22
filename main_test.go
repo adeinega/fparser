4733c4da61df7e4cf5acb46124895d636e050772
adeinega <andrii.deinega@bmc.com>
2024-10-22 10:04:34 -0700

    Mnr changes.

--- main_test.go
+++ main_test.go
@@ -16,9 +16,15 @@ func TestBuildLookupTable(t *testing.T) {
 	}{
 		{"1", "25,tcp,sv_P1", []tag{{dstPort: 25, protocol: 6, name: "sv_p1"}}, nil},
 		{"2", "# comment", []tag{}, nil},
-		{"3", "65536,tcp,sv_P1", nil, errors.New("invalid port 65536")},
-		{"4",
-			`25,tcp,sv_P1
+		{"3", "1,2,3,4,5", nil,
+			errors.New("record on line 1: wrong number of fields")},
+		{"4", "65536,tcp,sv_P1", nil,
+			errors.New("invalid port 65536")},
+		{"5", "22,bla,sv_P1", nil,
+			errors.New("invalid protocol number: bla")},
+		{"6",
+			`# comment
+25,tcp,sv_P1
 68,udp,sv_P2
 `,
 			[]tag{{dstPort: 25, protocol: 6, name: "sv_p1"},
