c47ce513b3709bd22ce013b939f7785f39963a76
Andrii Deinega <andrii.deinega@gmail.com>
2024-10-22 01:02:06 -0700

    An initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+This tiny GoLang app has zero deps. Note, package [encoding/csv](https://pkg.go.dev/encoding/csv@go1.23.2) is a part of [Standard library](https://pkg.go.dev/std).
+
+How to run it (this isn't a typical way to run GoLang apps)
+
+1. `clone the repo`
+2. run
+`docker run -it --entrypoint sh -v ${PWD}/:/usr/src -w /usr/src golang:1.23.2-alpine3.20`
+3. and then run `go run .`
+
+The app takes two CSV files ([lookup.csv](lookup.csv) and [flow.csv](flow.csv)) and generates a report to stdout.
+
+Any errors in lookup.csv force an app to quit.
+Minor errors in flow.csv are ignored and get printed out to stdout (they go to stderr).
+
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const FEILD_DST_PORT = 6
+const FIELD_PROTOCOL = 7
+
+type tag struct {
+	dstPort  int
+	protocol int
+	name     string
+	count    int
+}
+
+// https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+var ianaProtocolNumbers = map[string]int{
+	"icmp": 1,
+	"tcp":  6,
+	"udp":  17,
+	"sctp": 132,
+}
+
+func main() {
+	lookupFile, err := os.Open("lookup.csv") // Might need to have additional restrictions for prod use.
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer lookupFile.Close()
+
+	tags, err := buildLookupTable(lookupFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	flowFile, err := os.Open("flow.csv") // May need to have additional restrictions for prod use.
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer flowFile.Close()
+
+	m, untagged := stat(flowFile, tags)
+
+	fmt.Fprintln(os.Stdout, "Tag | Count")
+	for n, v := range m {
+		fmt.Fprintf(os.Stdout, "%s %d\n", n, v)
+	}
+	fmt.Fprintf(os.Stdout, "Untagged = %d\n", untagged)
+
+	fmt.Fprintln(os.Stdout, "Port | Protocol | Count")
+	for _, tag := range tags {
+		if tag.count > 0 {
+			fmt.Fprintf(os.Stdout, "%d %d %d\n", tag.dstPort, tag.protocol, tag.count)
+		}
+	}
+}
+
+func buildLookupTable(r io.Reader) ([]tag, error) {
+	parser := csv.NewReader(r)
+	parser.TrimLeadingSpace = true
+	parser.FieldsPerRecord = 3
+	parser.Comment = '#'
+
+	records, err := parser.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	a := make([]tag, len(records))
+	for i, record := range records {
+		port, err := strconv.Atoi(record[0])
+		if err != nil {
+			return nil, err
+		}
+		a[i].dstPort = port
+
+		if v, exists := ianaProtocolNumbers[strings.ToLower(record[1])]; !exists {
+			return nil, fmt.Errorf("invalid protocol number: %s", record[1])
+		} else {
+			a[i].protocol = v
+		}
+
+		a[i].name = strings.ToLower(record[2]) // Tag names are case-insensitive.
+	}
+	return a, nil
+}
+
+func stat(r io.Reader, tags []tag) (map[string]int, int) {
+	parser := csv.NewReader(r)
+	parser.Comma = ' ' // flow.csv doesn't use the comma as the field delimiter.
+	parser.TrimLeadingSpace = true
+
+	m := map[string]int{}
+	untagged := 0
+
+	for {
+		record, err := parser.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue // Should be OK to be a bit more resilient (TBD).
+		}
+
+		port, err := strconv.Atoi(record[FEILD_DST_PORT])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue //  Should be OK to be a bit more resilient (TBD).
+		}
+
+		protocol, err := strconv.Atoi(record[FIELD_PROTOCOL])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue //  Should be OK to be a bit more resilient (TBD).
+		}
+
+		marked := false
+		for i, t := range tags {
+			if t.dstPort == port && t.protocol == protocol {
+				// For Name | Count
+				if _, exists := m[t.name]; !exists {
+					m[t.name] = 1
+				} else {
+					m[t.name]++
+				}
+
+				// For Port | Protocol | Count
+				pTag := &tags[i]
+				(*pTag).count++
+
+				marked = true
+			}
+		}
+
+		if !marked {
+			untagged++
+		}
+	}
+	return m, untagged
+}
